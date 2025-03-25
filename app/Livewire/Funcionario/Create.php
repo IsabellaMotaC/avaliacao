af3358400ae82266a3856be34fa3b75d04705690
IsabellaMotaC <isabellamotacarvalho@hotmail.com>
2025-03-25 15:23:02 -0300

    atualizações

---
+++ app/Livewire/Funcionario/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Funcionario;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.funcionario.create');
+    }
+}
---
+++ app/Livewire/Funcionario/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Funcionario;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.funcionario.index');
+    }
+}
